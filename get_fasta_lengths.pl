6f694958ef1fbf7469f3f1aac34a7d8b1932f54f
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-10-27 14:36:32 -0400

    Update get_fasta_lengths.pl
    rename GNC... to NC12_Genomic

--- get_fasta_lengths.pl
+++ get_fasta_lengths.pl
@@ -3,21 +3,21 @@
 
 #use strict;
 
-my $GCA_000182925.2_NC12_genomic.fa;
+my $NC12_Genomic.fa;
 
 if ($ARGV[0]) {
-	"$GCA_000182925.2_NC12_genomic.fa" = $ARGV[0];
+	"$NC12_Genomic.fa" = $ARGV[0];
 }
 else {
 	print "Not enough arguments\n\n";
 	die "!\n\n";
 }
 
-my "$Ncrassa_Len.fa.fai"="$GCA_000182925.2_NC12_genomic.fa";
+my "$Ncrassa_Len.fa.fai"="$NC12_Genomic.fa";
 "$Ncrassa_Len.fa.fai" =~ s/.*\\//;
 "$Ncrassa_Len.fa.fai" =~ s/.*\///;
 "$Ncrassa_Len.fa.fai"="Ncrassa"."$Ncrassa_Len.fa.fai";
-open (FILE, "<$GCA_000182925.2_NC12_genomic.fa.fai") or die "Cannot open file!!!!: $!";
+open (FILE, "<$NC12_Genomic.fa") or die "Cannot open file!!!!: $!";
 open (OUT, ">$Ncrassa_Len.fa.fai") or die "Cannot open file!!!!: $!";
 
 $_ = <FILE>;
