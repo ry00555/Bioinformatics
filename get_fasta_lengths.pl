f9a4ce1e8ac410ff03d3920eb858d792d62e43ce
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-10-27 14:10:03 -0400

    Add more scripts
    Calculate N crassa length

---
+++ get_fasta_lengths.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl -w
+#prints lengths of sequences from a multifasta file
+
+use strict;
+
+my $\GCA_000182925.2_NC12_genomic.fa.fai;
+
+if ($ARGV[0]) {
+	$GCA_000182925.2_NC12_genomic.fa.fai = $ARGV[0];
+}
+else {
+	print "Not enough arguments\n\n";
+	die "!\n\n";
+}
+
+my $NcrassaWG.fa.fai=$GCA_000182925.2_NC12_genomic.fa.fai;
+$NcrassaLen.fa.fai =~ s/.*\\//;
+$NcrassaLen.fa.fai =~ s/.*\///;
+$NcrassaLen.fa.fai="Ncrassa".$NcrassaLen.fa.fai;
+open (FILE, "<$GCA_000182925.2_NC12_genomic.fa.fai") or die "Cannot open file!!!!: $!";
+open (OUT, ">$NcrassaLen.fa.fai") or die "Cannot open file!!!!: $!";
+
+$_ = <FILE>;
+if (/>(.*)/) {
+	 print OUT $1,"\t";
+}
+my $length = 0;
+
+while (<FILE>)  {
+	chomp;
+	if (/>(.*)/) {
+		print OUT $length,"\n";
+		$length = 0;
+		 print OUT $1,"\t";
+	}
+	else {
+		$length += length($_);
+	}
+}
+
+print OUT $length,"\n";
+
+close FILE;
+close OUT;
