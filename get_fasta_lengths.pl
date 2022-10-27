425150dfb0157c206b66a32c4ab9223746975a92
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-10-27 14:32:03 -0400

    Update get_fasta_lengths.pl

--- get_fasta_lengths.pl
+++ get_fasta_lengths.pl
@@ -3,7 +3,7 @@
 
 #use strict;
 
-my $GCA_000182925.2_NC12_genomic.fa;
+my "$GCA_000182925.2_NC12_genomic.fa";
 
 if ($ARGV[0]) {
 	"$GCA_000182925.2_NC12_genomic.fa" = $ARGV[0];
@@ -16,7 +16,7 @@ else {
 my "$Ncrassa_Len.fa.fai"="$GCA_000182925.2_NC12_genomic.fa";
 "$Ncrassa_Len.fa.fai" =~ s/.*\\//;
 "$Ncrassa_Len.fa.fai" =~ s/.*\///;
-"$Ncrassa_Len.fa.fai"="Ncrassa".$Ncrassa_Len.fa.fai;
+"$Ncrassa_Len.fa.fai"="Ncrassa"."$Ncrassa_Len.fa.fai";
 open (FILE, "<$GCA_000182925.2_NC12_genomic.fa.fai") or die "Cannot open file!!!!: $!";
 open (OUT, ">$Ncrassa_Len.fa.fai") or die "Cannot open file!!!!: $!";
 
