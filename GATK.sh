e7bdae35af1740335062fe4719c877384bd74eed
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-11-16 18:13:55 -0500

    Try GATK

---
+++ GATK.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=j_GATK
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ry00555@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH --time=08:00:00
+#SBATCH --output=GATK.%j.out
+#SBATCH --error=GATK.%j.err
+
+cd $SLURM_SUBMIT_DIR
+ml GATK/4.2.5.0-GCCcore-8.3.0-Java-1.8.lua
+java -jar $EBROOTGATK/GenomeAnalysisTK.jar [options]
+
+
+gatk CollectReadCounts \
+          -I /Users/ry00555/Desktop/RochelleLabDesktopData/IGV/mus30xmei3/SortedBamFiles/109_58_Genomic.bam \
+          -L intervals.interval_list \
+          --interval-merging-rule OVERLAPPING_ONLY \
+          -O 109_58.counts.tsv
