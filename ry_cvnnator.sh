e958700e798d2086fae73b1f696b9e2e38a2733a
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-03-01 14:09:42 -0500

    Update ry_cvnnator.sh

--- ry_cvnnator.sh
+++ ry_cvnnator.sh
@@ -20,23 +20,23 @@ OUTDIR= "/scratch/ry00555/Bioinformatics/CNVator"
 module spider CNVnator/0.4.1-foss-2019b-ROOT-6.14.06
 
 FILE=$1
-SbPTH='/Users/ry00555/Desktop/RochelleLabDesktopData/IGV/mus30xmei3/Bamfiles'
+BAMFILE='/Users/ry00555/Desktop/RochelleLabDesktopData/IGV/mus30xmei3/Bamfiles/'
 REF='/Users/ry00555/Desktop/NeurosporaGenome/GCF_000182925.2.fasta'
--d CHROM='/Users/ry00555/Desktop/NeurosporaGenome/Chromsomes/*.fa'
+CHROM='/Users/ry00555/Desktop/NeurosporaGenome/Chromsomes/Chr*.fa'
 
 #Stop at any error
 
 cd /scratch/ry00555/Bioinformatics/CNVator
 set -ue
 #EXTRACTING READ MAPPING FROM BAM/SAM FILES
-cnvnator -root ${FILE}.root -chrom $CHROM -tree ${SbPTH}/${FILE}*.bam
+cnvnator -root ${FILE}.root -chrom $CHROM -tree -d $REF $BAMFILE*.bam
 
-  cnvnator -root ${FILE}.root -chrom $CHROM -his 1670 -fasta ${REF}
-  cnvnator -root ${FILE}.root -chrom $CHROM -stat 1670
-  cnvnator -root ${FILE}.root -chrom $CHROM -eval 1670
-  cnvnator -root ${FILE}.root -chrom $CHROM -partition 1670
+  cnvnator -root ${FILE}.root -chrom $CHROM -his 1670 -fasta -d $REF
+  cnvnator -root ${FILE}.root -chrom $CHROM -stat 1670 -d $REF
+  cnvnator -root ${FILE}.root -chrom $CHROM -eval 1670 -d $REF
+  cnvnator -root ${FILE}.root -chrom $CHROM -partition 1670 -d $REF
  #cnvnator -root ${FILE}.root -genotype 1670
  #cnvnator -root ${FILE}.root -view 1670
- cnvnator -root ${FILE}.root -chrom $CHROM -call 1670 > ${FILE}_cnv_longbin
+ cnvnator -root ${FILE}.root -chrom $CHROM -call 1670 > ${BAMFILE}_cnv_longbin
 
  #First time I ran CNVnator I ran a smaller bin size (75). It will be run a second time but now with a bigger bin size 1670 (average gene lenght)
