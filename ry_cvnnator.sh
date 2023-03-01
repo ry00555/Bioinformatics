e17fa51f7750630882850bba0d3f6ac2f96b892d
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-03-01 12:49:17 -0500

    Update ry_cvnnator.sh

--- ry_cvnnator.sh
+++ ry_cvnnator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=j_GATK
+#SBATCH --job-name=j_CNVator
 #SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
@@ -16,26 +16,22 @@ cd $SLURM_SUBMIT_DIR
 
 ##make output directory
 OUTDIR= "/scratch/ry00555/Bioinformatics/CNVator"
-if [ ! -d $OUTDIR ]
-then
-mkdir -p $OUTDIR
-fi
+
 module spider CNVnator/0.4.1-foss-2019b-ROOT-6.14.06
 
 FILE=$1
-SbPTH='/Users/ry00555/Desktop/RochelleLabDesktopData/IGV/mus30xmei3/mus30Samples'
+SbPTH='/Users/ry00555/Desktop/RochelleLabDesktopData/IGV/mus30xmei3/mus30Samples/'
 REF='/Users/ry00555/Desktop/NeurosporaGenome/GCA_000182925.2_NC12_genomic.fasta'
 #Stop at any error
 set -ue
 #EXTRACTING READ MAPPING FROM BAM/SAM FILES
 cnvnator -root ${FILE}.root -tree ${SbPTH}/${FILE}_Genomic.bam
-
- cnvnator -root ${FILE}.root -his 1670 -fasta ${REF}
- cnvnator -root ${FILE}.root -stat 1670
- cnvnator -root ${FILE}.root -eval 1670
- cnvnator -root ${FILE}.root -partition 1670
+cnvnator -root ${FILE}.root -his 1670 -fasta ${REF}
+cnvnator -root ${FILE}.root -stat 1670
+cnvnator -root ${FILE}.root -eval 1670
+cnvnator -root ${FILE}.root -partition 1670
  #cnvnator -root ${FILE}.root -genotype 1670
  #cnvnator -root ${FILE}.root -view 1670
- cnvnator -root ${FILE}.root -call 1670 > ${FILE}_cnv_longbin
+cnvnator -root ${FILE}.root -call 1670 > ${FILE}_cnv_longbin
 
  #First time I ran CNVnator I ran a smaller bin size (75). It will be run a second time but now with a bigger bin size 1670 (average gene lenght)
