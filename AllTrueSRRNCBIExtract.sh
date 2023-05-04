c5ba6c963ab0cf1ecb782d43c0281b323333678c
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-05-04 15:05:45 -0400

    Update AllTrueSRRNCBIExtract.sh
    update paths so all are run from scratch/Bioinformatics

--- AllTrueSRRNCBIExtract.sh
+++ AllTrueSRRNCBIExtract.sh
@@ -11,16 +11,25 @@
 #SBATCH --output=../SRRtoGENE.%j.out
 #SBATCH --error=../SRRtoGENE.%j.err
 
-# create a directory for the sample names
-mkdir /Users/ry00555/Desktop/RochelleLabDesktopData/RNAseq/OLDmappedJGIcounts/SampleNames/
+cd /scratch/ry00555/Bioinformatics/
 
-File = '/Users/ry00555/Desktop/RochelleLabDesktopData/RNAseq/OLDmappedJGIcounts/SRRfromMetaBatch.txt'
+# Path to the file containing the list of SRR IDs
+SRR_FILE="SRRfromMetaBatch.txt"
 
-# create a new file to save the sample names
-touch /Users/ry00555/Desktop/RochelleLabDesktopData/RNAseq/OLDmappedJGIcounts/SampleNames/sample_names.txt
+# Path to the file to write the sample names to
+SAMPLE_NAMES_FILE="sample_names.txt"
 
-# loop through each SRR ID in the list
-while read srr; do
-  # extract the sample name using vdb-dump and append it to the sample_names.txt file
-  vdb-dump -S -f tab -C SAMPLE ~/ncbi/public/sra/SRR/$srr/$srr.sra | awk '{print $2}' >> /Users/ry00555/Desktop/RochelleLabDesktopData/RNAseq/OLDmappedJGIcounts/SampleNames/sample_names.txt
-done < /Users/ry00555/Desktop/RochelleLabDesktopData/RNAseq/OLDmappedJGIcounts/SRRfromMetaBatch.txt
+# Loop through the SRR IDs in the file
+while read SRR_ID; do
+  # Download the HTML content of the SRA website for the SRR ID
+  HTML=$(wget -qO- "https://www.ncbi.nlm.nih.gov/sra/?term=$SRR_ID")
+
+  # Extract the sample name from the HTML content using grep
+  SAMPLE_NAME=$(echo "$HTML" | grep -oP '(?<=<th scope="row">Sample:</th><td>)[^<]+' | head -1)
+
+  # Print the SRR ID and sample name to the console
+  echo "$SRR_ID, $SAMPLE_NAME"
+
+  # Append the SRR ID and sample name to the sample names file
+  echo "$SRR_ID, $SAMPLE_NAME" >> "sample_names.txt"
+done < "$SRR_FILE"
